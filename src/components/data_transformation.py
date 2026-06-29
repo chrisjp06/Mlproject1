4517d64d9e47059be9949e274a24b5d7d192f7ee
chrisjp06 <chrisjp2506@gmail.com>
2026-06-29 14:24:12 +0530

    Model training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -80,7 +80,6 @@ class DataTransformation:
             return (
                 train_arr,
                 test_arr,
-                self.data_transformation_config.preprocessor_obj_file_path
             )
         except Exception as e:
             raise CustomException(e, sys) 
